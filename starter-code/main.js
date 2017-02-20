adfe65e9530d2697bf1634c59fd1ae0ec21aa18e
Peter Hironaka <peterhironaka@mac.com>
2017-02-19 22:34:46 -0800

    You found a match

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,12 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+	if (cardOne === cardThree) {
+alert("You found a match!");
+} else if (cardTwo === cardThree) {
+	alert("Sorry, try again.");
+}
+
+
+
